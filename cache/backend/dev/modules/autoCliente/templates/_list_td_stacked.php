7b234f532dd042e015dad71f8cd44115847b2f5d
Almando <almando@gmail.com>
2010-05-29 20:01:54 -0300

    limpando cache

--- cache/backend/dev/modules/autoCliente/templates/_list_td_stacked.php
+++
@@ -1,3 +0,0 @@
-<td colspan="5">
-  <?php echo __('%%Codigo%% - %%Nome%% - %%Telefone%% - %%Transversalinicial%% - %%Transversalfinal%%', array('%%Codigo%%' => $cliente->getCodigo(), '%%Nome%%' => link_to($cliente->getNome(), 'cliente_edit', $cliente), '%%Telefone%%' => $cliente->getTelefone(), '%%Transversalinicial%%' => $cliente->getTransversalinicial(), '%%Transversalfinal%%' => $cliente->getTransversalfinal()), 'messages') ?>
-</td>
--- cache/backend/dev/modules/autoCliente/templates/_list_th_stacked.php
+++
@@ -1,1 +0,0 @@
-<?php include_partial('cliente/list_th_tabular', array('sort' => $sort)) ?>
